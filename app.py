2006644b7c5ee190c414dee116d64ad92922c5ac
TDRauv <utriaricardo8@gmail.com>
2026-09-13 17:57:42 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def inicio():
+    return jsonify({
+        "mensaje": "API de monitoreo ambiental funcionando",
+        "estado": "OK"
+    })
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
